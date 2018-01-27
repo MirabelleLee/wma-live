46a841b8c39120f442cf12d6626fa08b83ab5655
wanght <wanght@bairuitech.com>
2018-01-27 19:13:16 +0800

    迁移到AnyChat

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function (e) {
+    console.log("初始场景：", e)
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
